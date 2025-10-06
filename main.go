03bfb6baebfc4ec0c04667ebddf266992b11b77d
Valery Semenenko <gearmobile@gmail.com>
2025-10-06 20:52:27 +0300

    feat: use flag

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
-	//
+	city := flag.String("city", "", "Город пользователя")
+	format := flag.Int("format", 1, "Формат вывода")
+
+	flag.Parse()
+
+	fmt.Println(*city)
+	fmt.Println(*format)
 }
